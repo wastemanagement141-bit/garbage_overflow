55219ccc2fe1f318868b0c92bd88f3e6df9e964d
6ixmindslabs <6ixmindslabs@gmail.com>
2026-02-14 17:27:09 +0530

    update backend to show status msg on GET

--- api/bin/update.js
+++ api/bin/update.js
@@ -15,6 +15,10 @@ export default async function handler(req, res) {
         return
     }
 
+    if (req.method === 'GET') {
+        return res.status(200).json({ message: 'Smart Waste API is running. Send POST requests to update bin status.' });
+    }
+
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
